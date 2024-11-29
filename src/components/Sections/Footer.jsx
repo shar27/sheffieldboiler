f8d94e80c33778c8d9d1abf42c5d70893dd908e1
Shar <81372629+shar27@users.noreply.github.com>
2024-11-29 11:24:11 +0000

    boiler updates

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
               © {getCurrentYear()} - <span className="purpleColor font13">Property Maintenance Liverpool</span> All Right Reserved
             </StyleP> */}
             <StyleP className="whiteColor font13">
-             <h6 className="whiteColor font13">Manchester Property Maintenance  is a
+             <h6 className="whiteColor font13">Manchester Boiler Installation  is a
              trading name for <br/> SWIFT UK PROPERTY INVESTMENTS LTD. <br/>
              Company number: 15244665
              
@@ -38,7 +38,7 @@ export default function Contact() {
       </div>
       <BottomFooter>
     
-      <div className="contact-info"><h4>📧 hello@propertymaintenancemanchester.co.uk</h4></div>
+      <div className="contact-info"><h4>📧 hello@manchesterboilerinstallation.co.uk</h4></div>
         <div><h4>📞 0333 360 6113</h4></div>
         
     
--- src/components/Sections/Header.jsx
+++ src/components/Sections/Header.jsx
@@ -15,13 +15,13 @@ export default function Header() {
       <LeftSide className="flexCenter">
         <div>
         <HeadingTag>
-            Property maintenance specialists covering Manchester, Liverpool & surrounding areas.
+            Boiler Installation Services in Manchester.
           </HeadingTag>
-          <h1 className="extraBold font60">Manchester Property Maintenance.</h1>
+          <h1 className="extraBold font60">Manchester Boiler Installation.</h1>
           
           
           <HeaderP className="font13 semiBold">
-          Looking for reliable property maintenance services in Manchester, Liverpool & surrouding areas
+          Looking for reliable Boiler Installation services in Manchester, Liverpool & surrouding areas
           Our experienced team offers comprehensive solutions for residential and commercial properties, 
           including building maintenance, repairs, and management. Trust us for efficient and affordable services tailored to landlords and property owners. 
           Contact us for emergency assistance and ensure your property's upkeep with our expert solutions.
